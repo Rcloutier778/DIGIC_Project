f98a1bd4ccc0f8e500eb173a70759de72b50cec3
Richard Cloutier <rxc1028@eng-2500-21.main.ad.rit.edu>
2018-11-18 15:33:35 -0500

    Added clk, reset. Implemented SE and SI

--- mlp_model.vhd
+++ mlp_model.vhd
@@ -12,6 +12,8 @@ entity mlp_model is
         Qm : integer; -- for Qm.m # of integer bits
         Qn : integer); --for Qm.n # of decimal bits.  Look at part 4 for better explanation
     port (
+        clk : in std_logic;
+        reset : in std_logic;
         SI : in std_logic; --scan chain input (serial input used to set all weights and bias values in network for testing)
         SE : in std_logic; --
         clk : in std_logic;
@@ -22,19 +24,15 @@ end mlp_model;
 
 architecture beh of mlp_model is
   
-    -- x is 2D integer array
-    --type x2 is array (0 to (N*(Qm+Qn+1))) of real;
-    --type x is array (0 to L) of x2;
-    type xtype is array (0 to H)  of std_logic_vector(1+Qm+Qn downto 0);
+    type xtype is array (0 to N+H+1)  of std_logic_vector(1+Qm+Qn downto 0);
     signal x : xtype := (others => (others => '0'));
     --Weights
-    type Wtype is array (0 to N+H+1, 0 to N+H+1) of std_logic_vector(1+Qm+Qn downto 0); --0,1, ... N,N+1, ... N+H+1
+    type Wtype is array (0 to N+H+1) of std_logic_vector(1+Qm+Qn downto 0); --0,1, ... N,N+1, ... N+H+1
     signal W : Wtype;    
-    type stype is array (1 to N+H+1) of std_logic_vector(1+Qm+Qn downto 0);-- := (others=> (others => '0'));
+    type stype is array (0 to N+H+1) of std_logic_vector(1+Qm+Qn downto 0);-- := (others=> (others => '0'));
     signal s : stype := (others => (others=>'0'));
-    type fstype is array (1 to N+H+1) of std_logic_vector(1+Qm+Qn downto 0);
+    type fstype is array (0 to N+H+1) of std_logic_vector(1+Qm+Qn downto 0);
     signal fs : fstype := (others => (others=> '0'));
-    --variable s : real := 0.0;
     
     component fast_sigmoid
         port(
@@ -49,31 +47,57 @@ begin
             generic map (Qn=>Qn, Qm=>Qm) 
             port map (s(I+N+1), fs(I+N+1)) ;
     end generate GEN_FAST_SIGMOID;
-    process (u)
+    
+    process (clk, reset, u, SE)
     variable a : integer; --loop 
     variable b : integer;
     variable tempS : signed(1+Qm+Qn downto 0) := (others=>'0');
+    variable W_Index_Counter : integer :=0;
     begin
-        --y=hw(u)
-        --x(1) = u
-        --x(L) = f((W^L)*x^(l-L))
-        -- (W^L)*x^(l-L) is a dot product, should produce a single value
-        --f(s) = 1/(1+e^(-s)) --sigmoid
-        --fast sigmoid: f(s) = s/(1+abs(s))
-        --TODO put all real into Qm.n format for performance?
-        
-        --activation function
-        --u has 1 signed bit, m integer bits, n decimal bits
-        x(0) <= std_logic_vector(signed(u));
-        --x(0)(a) := Real(to_integer(unsigned'('0' & u(a))));
-        for a in 1 to H loop
-            --s = --W^L DOT x^(l-L)
-            tempS := (others=>'0');
-            for b in 1 to N+H+1 loop            
-                tempS := tempS + (signed(x(a-1)) * signed(W(N+a,b)));
-            s(a) <= std_logic_vector(tempS);
-            x(a) <= fs(a);
-            end loop;
-        end loop;
+        if (reset = '0') then
+            --Reset is just power input 
+            x <= (others => (others => '0'));
+            W <= (others => (others => '0'));
+            s <= (others => (others => '0'));
+            fs <= (others => (others => '0'));
+            W_Index_Counter := 0;
+        else
+            if (clk = '1' and clk'event) then
+                --Rising clock edge
+                if (SE = '1') then
+                    --Read in weights from SI, 1 bit at a time
+                    if (W_Index_Counter <= to_Integer(N+H+1)) then
+                        --Check to make sure it doesn't overrun index bounds
+                        W(W_Index_Counter) = SI;
+                        W_Index_Counter = W_Index_Counter + 1;
+                    end if;
+                else
+                    --y=hw(u)
+                    --x(1) = u
+                    --x(L) = f((W^L)*x^(l-L))
+                    -- (W^L)*x^(l-L) is a dot product, should produce a single value
+                    --f(s) = 1/(1+e^(-s)) --sigmoid
+                    --fast sigmoid: f(s) = s/(1+abs(s))
+                    --TODO put all real into Qm.n format for performance?
+                    
+                    --activation function
+                    --u has 1 signed bit, m integer bits, n decimal bits
+                    for a in 0 to N loop
+                        --All inputs and the bias of H are u, rest use sigmoid
+                        x(a) <= std_logic_vector(signed(u(((N-a)*(Qm+Qn+1)) downto ((N-1-a)*(Qm+Qn+1)))));
+                    end loop;
+                    --TODO: Parallelize the dot product calc?
+                    for a in N+1 to N+H+1 loop
+                        --s = --W^L DOT x^(l-L)
+                        tempS := (others=>'0');
+                        for b in 1 to N+H+1 loop            
+                            tempS := tempS + (signed(x(a-1)) * signed(W(a,b)));
+                        s(a) <= std_logic_vector(tempS);
+                        x(a) <= fs(a);
+                        end loop;
+                    end loop;
+                end if;
+            end if;
+        end if;
     end process;
 end architecture beh;
