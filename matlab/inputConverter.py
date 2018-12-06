a1e7e89cd103d326240f701926374a0c2b5e20e8
Richard Cloutier <rxc1028@eng-2500-12.main.ad.rit.edu>
2018-12-06 12:25:23 -0500

    Added input conversion

--- matlab/inputConverter.py
+++ matlab/inputConverter.py
@@ -6,8 +6,12 @@ u=""
 with open(filestr+'.txt','r') as f:
     lines = f.readlines()
     for line in lines:
-        print(len(line.split("\t")))
         for dig in line.split("\t"):
+            if int(dig) > 0:
+                u+='0'
+            else:
+                print("WARNING: An input was negative. I don't account for this")
+
             u += "{0:b}".format(int(dig))
             u += "\n"
 u = u[:-2]
