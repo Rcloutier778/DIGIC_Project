2e32727331993c8daa1fa3394030bec2fbdda15b
Richard Cloutier <rcloutier778@gmail.com>
2018-12-12 12:28:13 -0500

    Add files via upload

--- matlab/top.m
+++ matlab/top.m
@@ -68,10 +68,10 @@ n = nnnew(nu,nh,ny,f,fp,wmax,wmin);
 [n traininfo] = nnrbp(n,T,T,epochs);
 
 % Plot the learning curve
-plot(1:epochs,mean(traininfo{1}'));
-xlabel('Epoch');
-ylabel('MSE');
-title('Training Error vs. Training Epoch (iteration)');
+% plot(1:epochs,mean(traininfo{1}'));
+% xlabel('Epoch');
+% ylabel('MSE');
+% title('Training Error vs. Training Epoch (iteration)');
 
 OldW = n.A.*n.W;
 counter = 1;
@@ -103,49 +103,49 @@ for i = 1:1+nu+nh+ny
         end
         s1 = num2str(pos);
         for QnnCounter = 1:length(d2b_num)
-            s2 = num2str(d2b_num(1,QnnCounter));
+            s2 = num2str(d2b_num(QnnCounter));
             s1 = strcat(s1,s2);
         end
         for QmmCounter = 1:length(d2b_dec)
-            s2 = num2str(d2b_num(1,QmmCounter));
+            s2 = num2str(d2b_dec(QmmCounter));
             s1 = strcat(s1,s2);
         end
-        s1 = string(s1);
-        FinalW(counter,1) = s1;
+        s1 = char(s1);
+        FinalW{counter,1} = s1;
         counter = counter + 1;
     end
 end
 
 % Write the weight matrix
-%dlmwrite('W.dat',FinalW);
+dlmwrite('old_W.dat',OldW);
 fileID = fopen('W.dat','wt');
 for (total = 1:31*31)
-    fprintf(fileID, '%s\n', FinalW(total));
+    fprintf(fileID, '%s\n', FinalW{total});
 end
 fclose(fileID);
 
-% Try it out on the images
-files = {'test1.txt' 'test2.txt' 'test3.txt' 'test4.txt'};
-figure;
-for file=1:4,
-  imin = load(files{file});
-  imin = (imin-mean(imin(:)))*75/std(imin(:))+128;
-  imin = 2.0*(imin/255.0-0.5);
-  G = im2col(imin',[3 3],'sliding');
-  imout = zeros(1,size(G,2));
-  for i=1:size(G,2),
-    n = nneval(n,G(:,i));
-    imout(i) = round(n.x(n.nx));
-  end
-  imout = reshape(imout,size(imin,1)-2,size(imin,2)-2)';
-  subplot(4,3,(file-1)*3+1);
-  imshow(mat2gray((imin+1.0)*255.0/2.0));
-  title('Original');
-  subplot(4,3,(file-1)*3+2);
-  imshow(mat2gray((imout+1.0)*255.0/2.0));
-  title('Neural Network');
-  subplot(4,3,(file-1)*3+3);
-  imshow(edge(mat2gray((imin+1.0)*255.0/2.0),'sobel'));
-  title('Sobel');
-end
+% % Try it out on the images
+% files = {'test1.txt' 'test2.txt' 'test3.txt' 'test4.txt'};
+% figure;
+% for file=1:4,
+%   imin = load(files{file});
+%   imin = (imin-mean(imin(:)))*75/std(imin(:))+128;
+%   imin = 2.0*(imin/255.0-0.5);
+%   G = im2col(imin',[3 3],'sliding');
+%   imout = zeros(1,size(G,2));
+%   for i=1:size(G,2),
+%     n = nneval(n,G(:,i));
+%     imout(i) = round(n.x(n.nx));
+%   end
+%   imout = reshape(imout,size(imin,1)-2,size(imin,2)-2)';
+%   subplot(4,3,(file-1)*3+1);
+%   imshow(mat2gray((imin+1.0)*255.0/2.0));
+%   title('Original');
+%   subplot(4,3,(file-1)*3+2);
+%   imshow(mat2gray((imout+1.0)*255.0/2.0));
+%   title('Neural Network');
+%   subplot(4,3,(file-1)*3+3);
+%   imshow(edge(mat2gray((imin+1.0)*255.0/2.0),'sobel'));
+%   title('Sobel');
+% end
 
