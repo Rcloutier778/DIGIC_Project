b84f98bfca26398e74ae200cd6a93eb8922305d6
Richard Cloutier <rxc1028@eng-2500-21.main.ad.rit.edu>
2018-12-13 12:43:47 -0500

    Redoing ordering of fast_sigmoid and matrix multiplication

--- fast_sigmoid.vhd
+++ fast_sigmoid.vhd
@@ -11,85 +11,76 @@ entity fast_sigmoid is
     generic(Qn : integer:=4;
             Qm : integer:=4);
     port(
-        s : in std_logic_vector(Qn+Qm downto 0);
-        fs : out std_logic_vector(Qn+Qm downto 0));
+        s : in std_logic_vector(Qn+Qm downto 0) := (others => '0');
+        fs : out std_logic_vector(Qn+Qm downto 0) := (others => '0'));
 end fast_sigmoid;
 
 architecture beh of fast_sigmoid is
-signal abs_s_int : unsigned(Qm downto 0);
-signal abs_s : unsigned(Qm+Qn downto 0);
+signal abs_s : unsigned(Qm+Qn downto 0) := (others =>'0');
 signal tfs : unsigned(Qn+Qm downto 0) := (others=>'0');
-signal tempSS : unsigned(2*(Qm+Qn+1)-1 downto 0) := (others=>'0');
-signal buf : std_logic_vector(Qn downto 0) := (others => '0');
---Conditional Statements
+signal tempS : unsigned(2*(Qm+Qn+1)-1 downto 0) := (others=>'0');
+constant buf : std_logic_vector(Qn downto 0) := (others => '0');
+	
+    --Conditional Statements
     --Case1 = 5.0
-    signal case1_int : std_logic_vector(Qm downto 0) := (3 => '1', 0=> '1', others => '0');
-    signal case1_dec : std_logic_vector(Qn-1 downto 0) := (others=>'0');
-    signal case1 : unsigned(Qn+Qm downto 0) := unsigned(case1_int(Qm downto 0) & case1_dec(Qn-1 downto 0));
+    constant case1_int : std_logic_vector(Qm+3 downto 0) := (3 => '1', 0=> '1', others => '0');
+    constant case1_dec : std_logic_vector(Qn-1 downto 0) := (others=>'0');
+    constant case1 : unsigned(Qn+Qm downto 0) := unsigned(case1_int(Qm downto 0) & case1_dec(Qn-1 downto 0));
     
     --Case2 = 2.375
-    signal case2_int : std_logic_vector(Qm downto 0) := (1 => '1', others => '0');
-    signal case2_dec : std_logic_vector(2 downto 0) := "011";
-    signal case2_dec_fin : std_logic_vector(Qn+3 downto 0) := (case2_dec & buf);
-    signal case2 : unsigned(Qn+Qm downto 0) := unsigned(case2_int(Qm downto 0) & case2_dec_fin(case2_dec_fin'Length-1 downto case2_dec_fin'Length-Qn));
+    constant case2_int : std_logic_vector(Qm+1 downto 0) := (1 => '1', others => '0');
+    constant case2_dec : std_logic_vector(2 downto 0) := "011";
+    constant case2_dec_fin : std_logic_vector(Qn+3 downto 0) := (case2_dec & buf);
+    constant case2 : unsigned(Qn+Qm downto 0) := unsigned(case2_int(Qm downto 0) & case2_dec_fin(case2_dec_fin'Length-1 downto case2_dec_fin'Length-Qn));
     
     --Case3 = 1.0
-    signal case3_int : std_logic_vector(Qm downto 0) := (0 => '1', others => '0');
-    signal case3_dec : std_logic_vector(Qn-1 downto 0) := (others => '0');
-    signal case3 : unsigned(Qn+Qm downto 0) := unsigned(case3_int & case3_dec);
+    constant case3_int : std_logic_vector(Qm downto 0) := (0 => '1', others => '0');
+    constant case3_dec : std_logic_vector(Qn-1 downto 0) := (others => '0');
+    constant case3 : unsigned(Qn+Qm downto 0) := unsigned(case3_int & case3_dec);
     
     --Case4 = 0.0
-    signal case4 : unsigned(Qn+Qm downto 0) := (others =>'0');
+    constant case4 : unsigned(Qn+Qm downto 0) := (others =>'0');
     
     --Multiplication
-    signal case1_mult_int : std_logic_vector(Qm downto 0) := (others => '0');
-    signal case1_mult_dec : std_logic_vector(4 downto 0) := "00001";
-    signal case1_mult_dec_fin : std_logic_vector(Qn+5 downto 0) := (case1_mult_dec & buf);
-    signal case1_mult : unsigned(Qn+Qm downto 0) := unsigned(case1_mult_int & case1_mult_dec_fin(case1_mult_dec_fin'Length-1 downto case1_mult_dec_fin'Length-Qn));
+    constant case1_mult_int : std_logic_vector(Qm downto 0) := (others => '0');
+    constant case1_mult_dec : std_logic_vector(4 downto 0) := "00001";
+    constant case1_mult_dec_fin : std_logic_vector(Qn+5 downto 0) := (case1_mult_dec & buf);
+    constant case1_mult : unsigned(Qn+Qm downto 0) := unsigned(case1_mult_int & case1_mult_dec_fin(case1_mult_dec_fin'Length-1 downto case1_mult_dec_fin'Length-Qn));
 
-    signal case2_mult_int : std_logic_vector(Qm downto 0) := (others => '0');
-    signal case2_mult_dec : std_logic_vector(2 downto 0) := "001";
-    signal case2_mult_dec_fin : std_logic_vector(Qn+3 downto 0) := (case2_mult_dec & buf);
-    signal case2_mult : unsigned(Qn+Qm downto 0) := unsigned(case2_mult_int & case2_mult_dec_fin(case2_mult_dec_fin'Length-1 downto case2_mult_dec_fin'Length-Qn));
+    constant case2_mult_int : std_logic_vector(Qm downto 0) := (others => '0');
+    constant case2_mult_dec : std_logic_vector(2 downto 0) := "001";
+    constant case2_mult_dec_fin : std_logic_vector(Qn+3 downto 0) := (case2_mult_dec & buf);
+    constant case2_mult : unsigned(Qn+Qm downto 0) := unsigned(case2_mult_int & case2_mult_dec_fin(case2_mult_dec_fin'Length-1 downto case2_mult_dec_fin'Length-Qn));
     
     
-    signal case3_mult_int : std_logic_vector(Qm downto 0) := (others => '0');
-    signal case3_mult_dec : std_logic_vector(1 downto 0) := "01";
-    signal case3_mult_dec_fin : std_logic_vector(Qn+2 downto 0) := case3_mult_dec & buf;
-    signal case3_mult : unsigned(Qn+Qm downto 0) := unsigned(case3_mult_int & case3_mult_dec_fin(case3_mult_dec_fin'Length-1 downto case3_mult_dec_fin'Length-Qn));
+    constant case3_mult_int : std_logic_vector(Qm downto 0) := (others => '0');
+    constant case3_mult_dec : std_logic_vector(1 downto 0) := "01";
+    constant case3_mult_dec_fin : std_logic_vector(Qn+2 downto 0) := case3_mult_dec & buf;
+    constant case3_mult : unsigned(Qn+Qm downto 0) := unsigned(case3_mult_int & case3_mult_dec_fin(case3_mult_dec_fin'Length-1 downto case3_mult_dec_fin'Length-Qn));
 
     --Addition
     
     --0.84375
-    signal case1_add_int : std_logic_vector(Qm downto 0) := (others => '0');
-    signal case1_add_dec : std_logic_vector(4 downto 0) := "11011";
-    signal case1_add_dec_fin : std_logic_vector(Qn+5 downto 0) := case1_add_dec & buf;
-    signal case1_add : unsigned(Qn+Qm downto 0) := unsigned(case1_add_int & case1_add_dec_fin(case1_add_dec_fin'Length-1 downto case1_add_dec_fin'Length-Qn));
+    constant case1_add_int : std_logic_vector(Qm downto 0) := (others => '0');
+    constant case1_add_dec : std_logic_vector(4 downto 0) := "11011";
+    constant case1_add_dec_fin : std_logic_vector(Qn+5 downto 0) := case1_add_dec & buf;
+    constant case1_add : unsigned(Qn+Qm downto 0) := unsigned(case1_add_int & case1_add_dec_fin(case1_add_dec_fin'Length-1 downto case1_add_dec_fin'Length-Qn));
     
     --0.625
-    signal case2_add_int : std_logic_vector(Qm downto 0) := (others => '0');
-    signal case2_add_dec : std_logic_vector(2 downto 0) := "101";
-    signal case2_add_dec_fin : std_logic_vector(Qn+3 downto 0) := case2_add_dec & buf;
-    signal case2_add : unsigned(Qn+Qm downto 0) := unsigned(case2_add_int & case2_add_dec_fin(case2_add_dec_fin'Length-1 downto case2_add_dec_fin'Length-Qn));
+    constant case2_add_int : std_logic_vector(Qm downto 0) := (others => '0');
+    constant case2_add_dec : std_logic_vector(2 downto 0) := "101";
+    constant case2_add_dec_fin : std_logic_vector(Qn+3 downto 0) := case2_add_dec & buf;
+    constant case2_add : unsigned(Qn+Qm downto 0) := unsigned(case2_add_int & case2_add_dec_fin(case2_add_dec_fin'Length-1 downto case2_add_dec_fin'Length-Qn));
     
     --0.5
-    signal case3_add_int : std_logic_vector(Qm downto 0) := (others => '0');
-    signal case3_add_dec : std_logic_vector(Qn-1 downto 0) := (Qn-1 => '1', others=>'0');
-    signal case3_add : unsigned(Qn+Qm downto 0) := unsigned(case3_add_int & case3_add_dec);
-begin
-	
-	
-    process (s)
-	
-    
+    constant case3_add_int : std_logic_vector(Qm downto 0) := (others => '0');
+    constant case3_add_dec : std_logic_vector(Qn-1 downto 0) := (Qn-1 => '1', others=>'0');
+    constant case3_add : unsigned(Qn+Qm downto 0) := unsigned(case3_add_int & case3_add_dec);
     
     begin
+    process(s, abs_s, tfs, tempS) 
+    begin
         abs_s <= unsigned(std_logic_vector(abs(signed(s(Qn+Qm downto 0)))));
-        --fs <= s/(1+s);
-        
-        --abs value of s
-        
-        --
         if (unsigned(abs_s) >= case1) then   
             --|X| >=5
             --Y=1
@@ -97,16 +88,16 @@ begin
         elsif (unsigned(abs_s) >= case2) and (unsigned(abs_s) < case1) then
             --2.375 <= |X| < 5
             --Y=0.03125 |X| + 0.84375
-            tempSS <= case1_mult * abs_s;
-			tfs <= case1_add + tempSS(2*(Qn+Qm)-1 downto Qm+Qn);
+            tempS <= case1_mult * abs_s;
+			tfs <= case1_add + tempS(2*(Qn+Qm)-1 downto Qm+Qn);
         elsif abs_s >= case3 and (abs_s < case2) then
             --1 <= |X| < 2.375
-            tempSS <= case2_mult * abs_s;
-			tfs <= case2_add + tempSS(2*(Qn+Qm)-1 downto Qm+Qn);
+            tempS <= case2_mult * abs_s;
+			tfs <= case2_add + tempS(2*(Qn+Qm)-1 downto Qm+Qn);
         elsif abs_s >= case4 and abs_s < case3 then
             --0 <= |X| < 1
-			tempSS <= case3_mult * abs_s;
-			tfs <= case3_add + tempSS(2*(Qn+Qm)-1 downto Qm+Qn);
+			tempS <= case3_mult * abs_s;
+			tfs <= case3_add + tempS(2*(Qn+Qm)-1 downto Qm+Qn);
         end if;
         if s(Qn+Qm) = '1' then
             fs <= std_logic_vector(signed(1-tfs(Qm+Qn downto 0)));
--- mlp_model.vhd
+++ mlp_model.vhd
@@ -14,23 +14,24 @@ entity mlp_model is
         Qn : integer :=4); --for Qm.n # of decimal bits.  Look at part 4 for better explanation
     port (
         clk : in std_logic;
-        reset : in std_logic;
-        SI : in std_logic; --scan chain input (serial input used to set all weights and bias values in network for testing)
-        SE : in std_logic; --
-        u : in std_logic_vector(N*(Qm+Qn+1)-1 downto 0);
-        yhat : out std_logic_vector(M*(Qm+Qn+1)-1 downto 0));
+        reset : in std_logic :='1';
+        SI : in std_logic := '0'; --scan chain input (serial input used to set all weights and bias values in network for testing)
+        SE : in std_logic :='0'; --
+        u : in std_logic_vector(N*(Qm+Qn+1)-1 downto 0) := (others => '0');
+        yhat : out std_logic_vector(M*(Qm+Qn+1)-1 downto 0) := (others =>'0'));
 
 end mlp_model;
 
 architecture beh of mlp_model is
   
-    type xtype is array (0 to N+H+1+M)  of std_logic_vector(Qm+Qn downto 0);
+    type xtype is array (0 to N+H+M)  of std_logic_vector(Qm+Qn downto 0);
     signal x : xtype := (others => (others => '0'));
     --Weights
     type Wtype is array (0 to N+H+M, 0 to N+H+M) of std_logic_vector(Qm+Qn downto 0); --0,1, ... N,N+1, ... N+H+1
-    signal W : Wtype;    
-    type stype is array (0 to N+H+1+M) of std_logic_vector(Qm+Qn downto 0);-- := (others=> (others => '0'));
+    signal W : Wtype := (others => (others => (others => '0')));    
+    type stype is array (0 to N+H+M) of std_logic_vector(Qm+Qn downto 0);-- := (others=> (others => '0'));
     signal s : stype := (others => (others=>'0'));
+    signal EOF_W : std_logic := '0'; --if end of W array has been reached
 	
     component fast_sigmoid
         generic( Qn : integer; Qm : integer);
@@ -38,64 +39,64 @@ architecture beh of mlp_model is
             s : in std_logic_vector(Qm+Qn downto 0);
             fs : out std_logic_vector(Qm+Qn downto 0));
     end component;
-begin
+    begin
     
     GEN_FAST_SIGMOID:
-    for I in 0 to H+M generate
+    for I in 0 to H+M-1 generate
         fast_sigmoidX : fast_sigmoid 
             generic map (Qn=>Qn, Qm=>Qm) 
-            port map (s=> s(I+N+1), fs => x(I+N+1)) ;
+            port map (s=> s(I+N+1), fs => x(I+N+1));
     end generate GEN_FAST_SIGMOID;
     
-    process (clk, reset, u, SE)
-    variable a : integer; --loop 
-    variable b : integer;
+    process (clk, reset, u, SE, W, x, s, EOF_W)
+    variable a : integer := 0; --loop 
+    variable b : integer := 0;
     variable tempS : signed(Qm+Qn downto 0) := (others=>'0');
     variable tempSS : signed(2*(Qm+Qn+1)-1 downto 0) := (others=>'0');
     variable W_Index_Counter : integer range 0 to Qm+Qn :=0;
     variable W_Node_Counter : integer range 0 to N+H+M :=0;
     variable W_Node_Counter2 : integer range 0 to N+H+M :=0;
     begin
-        if (reset = '0') then
-            --Reset is just power input 
-            x <= (others => (others => '0'));
-            for a in 0 to N+H+M loop
-                for b in 0 to N+H+M loop
-                    W(a,b) <= (others => '0');
+        --Rising clock edge
+        if (clk = '1' and clk'event) then
+            if (reset = '0') then
+                --Reset is just power input 
+                x <= (others => (others => '0'));
+                for a in 0 to N+H+M loop
+                    for b in 0 to N+H+M loop
+                        W(a,b) <= (others => '0');
+                    end loop;
                 end loop;
-            end loop;
-            s <= (others => (others => '0'));
-            W_Index_Counter := 0;
-            W_Node_Counter := 0;
-            W_Node_Counter2 := 0;
-            yhat <= (others => '0');
-            
-        else
-            if (clk = '1' and clk'event) then
-                --Rising clock edge
+                s <= (others => (others => '0'));
+                W_Index_Counter := 0;
+                W_Node_Counter := 0;
+                W_Node_Counter2 := 0;
+                yhat <= (others => '0');
+                EOF_W <= '0';
+            else
                 if (SE = '1') then
-                    --Read in weights from SI, 1 bit at a time
-                    if (W_Node_Counter <= N+H+M) then
-                        --Check to make sure it doesn't overrun index bounds
+                    if EOF_W = '0' then
+                        --Read in weights from SI, 1 bit at a time
                         W(W_Node_Counter,W_Node_Counter2)(W_Index_Counter) <= SI;
-                        if W_Index_Counter = Qm+Qn then
-                            W_Index_Counter := 0;
-                            --If at end of Node, go to next node and start at 0
-                            if W_Node_Counter2 = N+H+M then
-                                if W_Node_Counter /= N+H+M then
-                                    W_Node_Counter := W_Node_Counter +1;
-                                    W_Node_Counter2 := 0;
-                                end if;
-                            else
-                                W_Node_Counter2 := W_Node_Counter2 +1;
-                            end if;
-                        else
-                            --Increment index
+                        if W_Index_Counter < Qm+Qn then
                             W_Index_Counter := W_Index_Counter + 1;
+                        else
+                            W_Index_Counter := 0;
+                            if W_Node_Counter2 < N+H+M then
+                                W_Node_Counter2 := W_Node_Counter2 + 1;
+                            else
+                                W_Node_Counter2 := 0;
+                                if W_Node_Counter < N+H+M then
+                                    W_Node_Counter := W_Node_Counter + 1;
+                                else
+                                    W_Node_Counter := 0;
+                                    EOF_W <= '1';
+                                end if;
+                            end if;
                         end if;
-                        
                     end if;
                 else
+                    EOF_W <= '0';
                     --y=hw(u)
                     --x(1) = u
                     --x(L) = f((W^L)*x^(l-L))
@@ -103,13 +104,15 @@ begin
                     --f(s) = 1/(1+e^(-s)) --sigmoid
                     --fast sigmoid: f(s) = s/(1+abs(s))
                     -- l is the layer
+
+                    --Input layer
                     for a in 0 to N-1 loop
                         --All inputs and the bias of H are u, rest use sigmoid
                         x(a) <= std_logic_vector(signed(u(((a+1)*(Qm+Qn+1))-1 downto (a*(Qm+Qn+1)))));
                     end loop;
                     
                     --Hidden layer
-                    for a in 0 to H+M loop --For each node in hidden layer
+                    for a in 0 to H loop --For each node in hidden layer
                         --s = --W^L DOT x^(l-L)
                         tempS := (others=>'0');
                         for b in 0 to N loop  --For weights 0 to N in the node 
@@ -121,16 +124,32 @@ begin
                             
                         end loop;
                         s(a+N) <= std_logic_vector(tempS);
-						
+					
+                        --ReLU
 						-- if tempS < 0 then
-							-- x(a) <= (others => '0');
+							-- x(a+N) <= (others => '0');
 						-- else
-							-- x(a) <= std_logic_vector(tempS);
+							-- x(a+N) <= std_logic_vector(tempS);
 						-- end if;
                     end loop;
                     
                     --Output layer
-                    for a in 1 to M loop --For each node in output layer
+                    for a in 1 to M loop --For each node in the output layer
+                        --s = --W^L DOT x^(l-L)
+                        tempS := (others=>'0');
+                        for b in 0 to H loop  --For each node in the hidden layer 
+                            tempSS := signed(W(a+N+H,b)) * signed(x(b));
+                            tempS := tempS + tempSS(2*(Qn+Qm)-1 downto Qm+Qn);
+                            
+                        end loop;
+                        s(a+N+H) <= std_logic_vector(tempS);
+					
+                        --ReLU
+						-- if tempS < 0 then
+							-- x(a+N+H) <= (others => '0');
+						-- else
+							-- x(a+N+H) <= std_logic_vector(tempS);
+						-- end if;
                         yhat(a*(Qm+Qn+1)-1 downto (a-1)*(Qm+Qn) ) <= x(a);
                     end loop;
                 end if;
